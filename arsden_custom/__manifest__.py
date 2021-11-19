dcc463c8236db476b09936202eb547416536d467
whilly <whillgall@gmail.com>
2021-11-19 10:31:50 -0400

    Proyecto ArsDen

---
+++ arsden_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Arseden',
+    'description': ''' Proyecto Arsden ''',
+    "version": "14.0.1.0.0",
+    "author": "Whilly G",
+    "website": "",
+    'category': 'account',
+    'depends': ['base', 'account', 'sale'],
+    'data': [
+        'views/arsden_custom.xml',
+        'views/arsden_account.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
---
+++ arsden_custom/models/__init__.py
@@ -0,0 +1,1 @@
+from . import arsden_custom
